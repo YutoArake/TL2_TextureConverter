bc7be88cd73b9b4478daf22d4620c37df1703a61
YutoArake <k021g1420@g.neec.ac.jp>
2023-10-03 14:48:56 +0900

    02_03_コマンド引数をセット

--- main.cpp
+++ main.cpp
@@ -1,10 +1,17 @@
 #include <cstdio>
 #include <cstdlib>
 
-int main() {
-	// hello,world!出力
-	printf("Hello,World!\n");
+int main(int argc	, char* argv[]) {
+	// argcの数だけ繰り返す
+	for (int i = 0; i < argc; i++) {
+		// 文字列argvの1番を表示
+		printf(argv[i]);
+		// 改行
+		printf("\n");
+	}
+
 	// ポーズ
 	system("pause");
+
 	return 0;
 }
